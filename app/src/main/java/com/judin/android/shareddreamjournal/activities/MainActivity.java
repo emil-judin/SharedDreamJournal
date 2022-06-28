a13f3deb0f25e442a91d89497181720e2e753923
emil-judin <em.judin@gmail.com>
2022-06-28 19:46:07 +0200

    Main activity

--- app/src/main/java/com/judin/android/shareddreamjournal/activities/MainActivity.java
+++ app/src/main/java/com/judin/android/shareddreamjournal/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package com.judin.android.shareddreamjournal.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import com.judin.android.shareddreamjournal.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
     private static final String TAG = "MainActivity";
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
